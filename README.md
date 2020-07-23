7264aa9ab4788674d469b97bb05d81245808ede5
phpstorm <aertas16@gmail.com>
2020-07-23 04:28:07 +0300

    README.md ve twitter linkleri güncellendi

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
+[![isy.concof.com](https://isy.concof.com/assets/img/fav-icon.png)](https://isy.concof.com)
+
+[isy.concof.com](https://isy.concof.com)
+
 ## İstanbul Sözleşmesi Yaşatır Profil Resim Güncelle
 
 İstanbul Sözleşmesi farkınadlığını arttırmak adına çorbaya bir tutam tuz amaçlı kar amacı olmayan bir projedir
 
 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/aertas/istanbul-sozlesmesi-yasatir/blob/master/LICENSE)
+
 ![PUSH Workflow](https://github.com/aertas/istanbul-sozlesmesi-yasatir/workflows/PUSH%20Workflow/badge.svg)
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-	<div class="container mt-5 text-center">
+	<div class="container my-5 text-center">
 		<div class="row row-cols-1">
 			<div class="col">
 				<h1>İstanbul Sözleşmesi Yaşatır</h1>
@@ -12,9 +12,23 @@
 				<div id="croppic"></div>
 			</div>
 			<div class="col pt-3">
-				<button role="button" type="button" class="btn btn-primary" id="upload-image">Profil Resminizi Yüklemek İçin Tıklayın</button>
+				<button role="button" type="button" class="btn btn-primary" id="upload-image">Profil Resminizi Yüklemek İçin Tıklayın
+				</button>
 			</div>
+			{{--
+			<div class="col pt-3 mt-5">
+				<p></p>
+				<a href="https://istanbulsozlesmesiyasatir.com/" target="_blank">İstanbul Sözleşmesi Yaşatır</a>
+			</div>
+			--}}
+			<div class="col pt-3 mt-5">
+				<a href="https://twitter.com/search?q=%23istanbulsozlesmesiyasatir&src=typeahead_click" class="twitlink" target="_blank">#İstanbulSözleşmesiYaşatır</a>
+				<a href="https://twitter.com/search?q=%23istanbulsozlesmesi&src=typeahead_click" class="twitlink" target="_blank">#İstanbulSözleşmesi</a>
+			</div>
+
 		</div>
 	</div>
+
+	<a href="https://github.com/aertas/istanbul-sozlesmesi-yasatir" target="_blank" class="git" title="Fork me on github">GitHub</a>
 
 @endsection
