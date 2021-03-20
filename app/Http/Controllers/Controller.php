c6bfcaec0ed7fcaa7315a6f53afd4b0649437352
phpstorm <aertas16@gmail.com>
2021-03-20 06:03:17 +0300

    removal of temporary pictures

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Artisan;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	public function index()
+	{
+		Artisan::call('clean:directories');
+
+		return view('index');
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'Controller@index');
 
 Route::post('/', 'Crop@Image');
 Route::get('/exp/{filename}', 'Crop@Export');
